42099c5dabf9fb3134b2120e348073ededd27d6e
Jamie <134149238+JCritch91@users.noreply.github.com>
2023-06-10 16:29:59 +0100

    Format styles.css for nicer UI Recode java.js to show final score as soon as score hits 5 Add "Choose a weapon" to java.js

--- java.js
+++ java.js
@@ -5,8 +5,12 @@ const currentScoreText = document.getElementById("current-score");
 const playerSelectionText = document.getElementById('player-selection');
 const compSelectionText = document.getElementById("comp-selection");
 const roundResultText = document.getElementById("round-result");
+const chooseWeapon = document.getElementById("choose-weapon");
+
 let compScore = 0;
 let playerScore = 0;
+chooseWeapon.textContent ="Choose Your Weapon!"
+
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -16,27 +20,15 @@ buttons.forEach((button) => {
 
 restart.addEventListener('click', restartGame);
 
+
+
 function initialise (playerSelection) {
+    chooseWeapon.textContent =""
     if (compScore === 5 || playerScore === 5)  {
-    
-        if (compScore > playerScore) {
-            finalScoreText.textContent= "Computer Wins " + compScore + " - " + playerScore
-            return
-        }
-        else if (playerScore > compScore) {
-            finalScoreText.textContent= "Player Wins " + playerScore + " - " + compScore
-            return
-        }
-        else {
-            finalScoreText.textContent= "Its a Draw " + compScore + " - " + playerScore
-            return
-        }
+        return
     }
     
     else {
-    console.log("Current score: Player " + playerScore + " Comp " + compScore);
-    currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
-
     const play = game(playerSelection)
     } 
 }
@@ -52,42 +44,54 @@ function game(playerSelection) {
     function playRound(playerSelection, compChoice)  {
 
         if (playerSelection === "Rock" && compChoice === "Rock") {
-            compScore ++;
-            playerScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "It's a draw!" 
         }
         else if (playerSelection === "Rock" && compChoice === "Paper") {
             compScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "Computer Wins! Paper beats Rock"
         }
         else if (playerSelection === "Rock" && compChoice === "Scissors") {
             playerScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "You Win! Rock beats Scissors"
         }
         else if (playerSelection === "Paper" && compChoice === "Paper") {
-            compScore ++;
-            playerScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "It's a draw!"
         }
         else if (playerSelection === "Paper" && compChoice === "Rock") {
             playerScore ++
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "You Win! Paper beats Rock"
         }
         else if (playerSelection === "Paper" && compChoice === "Scissors") {
             compScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "Computer Wins! Scissors beat Paper"
         }
         else if (playerSelection === "Scissors" && compChoice === "Paper") {
             playerScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "You Win! Scissors beats Paper"
         }
         else if (playerSelection === "Scissors" && compChoice === "Rock") {
             compScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return  "Computer Wins! Rock beats Scissors"
         }
         else if (playerSelection === "Scissors" && compChoice === "Scissors") {
-            compScore ++;
-            playerScore ++;
+            currentScoreText.textContent = "Current score: Player " + playerScore + " Comp " + compScore
+            checkScore();
             return "It's a draw!"
         }
     }
@@ -119,4 +123,33 @@ function restartGame() {
     playerSelectionText.textContent = ""
     compSelectionText.textContent = ""
     roundResultText.textContent = ""
+    chooseWeapon.textContent ="Choose Your Weapon!"
+}
+
+function checkScore() {
+    if (compScore === 5 || playerScore === 5)  {
+    
+        if (compScore > playerScore) {
+            finalScoreText.textContent= "Computer Wins " + compScore + " - " + playerScore
+            playerSelectionText.textContent = "Player Selected " + playerSelection;
+            compSelectionText.textContent = "Computer Selected " + compChoice
+            return
+        }
+        else if (playerScore > compScore) {
+            finalScoreText.textContent= "Player Wins " + playerScore + " - " + compScore
+            playerSelectionText.textContent = "Player Selected " + playerSelection;
+            compSelectionText.textContent = "Computer Selected " + compChoice
+            return
+        }
+        else {
+            playerSelectionText.textContent = "Player Selected " + playerSelection;
+            compSelectionText.textContent = "Computer Selected " + compChoice
+            finalScoreText.textContent= "Its a Draw " + compScore + " - " + playerScore
+            return
+        }
+    }
+    
+    else {
+    return
+    } 
 }
